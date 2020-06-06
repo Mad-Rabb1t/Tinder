f59f5aac3d4a526ddcd9d067253154fc83a8851b
rzayev1213 <rzayev1213@gmail.com>
2020-06-06 05:43:48 +0400

    Test user added

--- src/main/resources/db/migration/V007__init_data.sql
+++ src/main/resources/db/migration/V007__init_data.sql
@@ -1,3 +1,4 @@
 insert into public.users (id, login, password, name, photo) values (1, 'ale', 'x', 'Alex', 'https://avatars0.githubusercontent.com/u/15745294?s=400&u=eeb6c145a5121066d892ae6abd68b51287a3dc1e&v=4');
 insert into public.users (id, login, password, name, photo) values (2, 'kam', 'ran', 'Kamran', 'https://cdn.mos.cms.futurecdn.net/z6ia6LxLhDKM62yiQwvEm3.jpg');
 insert into public.users (id, login, password, name, photo) values (3, 'tur', 'kan', 'Turkan', 'https://catsdogsvideo.com/wp-content/uploads/2017/03/maxresdefault-248.jpg');
+insert into public.users (id, login, password, name, photo) values (4, 'test', 'test123', 'Test User', '');
